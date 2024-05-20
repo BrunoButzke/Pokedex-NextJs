595815a81f464d68e38eb963622963155baf1040
Bruno <brunobutzkesouza@gmail.com>
2024-05-20 04:09:47 -0300

    Add 'more info' button

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 "use client"
-
-import { useRef, useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react';
 
 const IndexPage = () => {
   const [currentIndex, setCurrentIndex] = useState(0); 
   const [items, setItens] = useState([]);
-  const [pokemonData, setPokemonData] = useState(null);
+  const [pokemonData, setPokemonData] = useState([]);
   const [genData, setGenData] = useState(1)
-  const [error, setError] = useState(null);
+  const [error, setError] = useState();
+  const [info, setInfo] = useState("Carregando...");
 
   const rangeGen = [[0,151],[151,100],[251,135],[386,107],[494,155]];
-
-  let btnStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
+  const btnStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,9 +21,10 @@ const IndexPage = () => {
           throw new Error('Erro ao carregar os dados do Pokémon');
         }
         const data = await response.json();
-        setPokemonData(data);
-        setItens(data.results)
-        setCurrentIndex(0)
+        setPokemonData(data.results);
+        setItens(data.results);
+        setCurrentIndex(0);
+        setInfo("Talvez em outra geração");
       } catch (error) {
         setError(error.message);
       }
@@ -34,30 +33,29 @@ const IndexPage = () => {
     fetchData();
   }, [genData]);
 
-  function handleInput(e) {
+  function handleInput(e: { target: { value: any; }; }) {
     const inputValue = e.target.value; 
     setItens(searchTable(inputValue));  
    }
 
-  function searchTable(value) {
-    const filteredData = [];
+  function searchTable(value: string) {
+    const filteredData: never[] = [];
   
     if (value.length === 0) {
-      return pokemonData.results; 
+      return pokemonData; 
     }
    
-    for (let i = 0; i < pokemonData.results.length; ++i) {
+    for (let i = 0; i < pokemonData.length; ++i) {
      const newValue = value.toLowerCase();
    
-     const user = pokemonData.results[i].name.toLowerCase();
+     const user = pokemonData[i].name.toLowerCase();
    
      if (user.includes(newValue)) {
-       filteredData.push(pokemonData.results[i]);
+       filteredData.push(pokemonData[i]);
      }
     }
     return filteredData;
    }
-   
 
   const handleNext = () => {
     setCurrentIndex((index) => (index === items.length - 1 ? 0 : index + 1)); 
@@ -95,25 +93,30 @@ const IndexPage = () => {
 
   return (
     <div className="bg-gradient-to-b from-cyan-500 to-white-500">
-      <div className="flex flex-col items-center justify-center h-screen bg-center bg-no-repeat bg-[url('../public/pokedexAlt.png')] min-h-96 min-w-96">
-        <div className="relative right-5">
+      <div className="flex flex-col items-center justify-center h-screen bg-center bg-no-repeat bg-[url('../public/pokedexAlt.png')]">
+        <div className="relative bottom-0 right-5">
           <div className="h-40 w-60">
             <div className="flex flex-col justify-center items-center">
-              {renderItems().length ? renderItems() : (error ?? <p>Talvez em outra geração</p>)}
+              {renderItems().length ? renderItems() : (error ?? info)}
             </div>
           </div>
           <div className="flex relative top-3 left-10">
             <input className="px-2 w-52 border-2 border-gray-700 rounded-md" onChange={handleInput} placeholder="Filtre seus pokémons"/>
           </div>
-          <div className="flex relative top-40 left-5">
-            <button className={`mr-4 ${btnStyle} py-2 px-4`} onClick={handlePrevious}>
+          <div>
+            <button className={`${btnStyle} py-2 px-4 w-52 float-right relative top-16 right-3`}>
+              Mais Informações
+            </button>
+          </div>
+          <div className="flex relative top-28 left-6">
+            <button className={`mr-2 ${btnStyle} py-2 px-4`} onClick={handlePrevious}>
               Anterior
             </button>
             <button className={`${btnStyle} py-2 px-4`} onClick={handleNext}>
               Próximo
             </button>
           </div>
-          <div className="flex relative top-40 left-3.5 mt-3 w-40 mx-auto pb-2">
+          <div className="flex relative top-36 left-4 mt-2 w-40 mx-auto pb-2">
             {renderButtons()}
           </div>
         </div>
